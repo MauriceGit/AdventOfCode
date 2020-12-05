56a4cc7fe10bb45b6df5639c0179aef217ff1a90
mauricegit <github@tollmien.de>
2020-12-05 11:09:53 +0100

    down to two lines :D

--- 2020/05.py
+++ 2020/05.py
@@ -4,12 +4,9 @@ from utility import *
 
 def main():
 
-    lines = open_data("05.data")
+    ids = {int(l.translate("".maketrans("FBLR", "0101")), 2) for l in open_data("05.data")}
 
-    ids = {int(l.replace("B", "1").replace("F", "0").replace("R", "1").replace("L", "0"), 2) for l in lines}
-
-    print(max(ids))
-    print(*{x for x in range(1024) if {x-1, x, x+1} & ids == {x-1, x+1}})
+    print(max(ids), *{x for x in range(1024) if {x-1, x, x+1} & ids == {x-1, x+1}})
 
 
 if __name__ == "__main__":
