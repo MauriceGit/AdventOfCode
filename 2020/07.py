6225990581a7cd68142a9f5e46398188c2cbe254
mauricegit <github@tollmien.de>
2020-12-07 06:50:54 +0100

    empty day 7

---
+++ 2020/07.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3.7
+
+from utility import *
+
+
+
+def main():
+
+    lines = open_data("07.data")
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+# solution for 07.01: ?
+# solution for 07.02: ?
