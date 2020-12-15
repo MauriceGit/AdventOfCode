0a3b8e286caee12b5e0dc9bbf3d97e43e3eae155
Maurice Tollmien <maurice.tollmien@build-linux>
2020-12-15 09:15:47 +0100

    optimised with proper dict and list. Solution now OK.

--- 2020/15.py
+++ 2020/15.py
@@ -2,39 +2,71 @@
 
 from utility import *
 
-def main():
+def main_dict():
 
     lines = ints(open_data("15.data")[0])
 
     pos = 0
-    numbers = defaultdict(list)
+    numbers = dict()
     last_num = 0
 
-    for i, n in enumerate(lines):
-        numbers[n].append(pos)
+    for i, n in enumerate(lines[:-1]):
+        numbers[n] = pos
         last_num = n
         pos = i+1
+    last_num = lines[-1]
+    pos = i+1
 
     while True:
 
-        if pos == 2020:
+        if pos+1 == 2020:
             print(last_num)
-        if pos == 30000000:
+        if pos+1 == 30000000:
             print(last_num)
             break
 
-        last = numbers[last_num]
         new = 0
-        if len(last) > 1:
-            new = last[-1]-last[-2]
+        if last_num in numbers:
+            new = pos - numbers[last_num]
 
-        numbers[new].append(pos)
+        numbers[last_num] = pos
+        last_num = new
+        pos += 1
+
+def main_list():
+
+    lines = ints(open_data("15.data")[0])
+
+    pos = 0
+    numbers = [-1]*30000000
+    last_num = 0
+
+    for i, n in enumerate(lines[:-1]):
+        numbers[n] = pos
+        last_num = n
+        pos = i+1
+    last_num = lines[-1]
+    pos = i+1
+
+    while True:
+
+        if pos+1 == 2020:
+            print(last_num)
+        if pos+1 == 30000000:
+            print(last_num)
+            break
+
+        new = 0
+        if numbers[last_num] != -1:
+            new = pos - numbers[last_num]
+
+        numbers[last_num] = pos
         last_num = new
         pos += 1
 
 
 if __name__ == "__main__":
-    main()
+    main_list()
 
 # solution for 15.01: 206
 # solution for 15.02: 955
