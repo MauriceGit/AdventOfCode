3f9fbcf8f448d8a6150bc464f8c5c9f1da70e895
Maurice <maurice.tollmimen@gmail.com>
2020-12-25 15:31:33 +0100

    documentation of what I did...

--- 2020/25.py
+++ 2020/25.py
@@ -2,37 +2,19 @@
 
 from utility import *
 
-
- # https://stackoverflow.com/questions/4798654/modular-multiplicative-inverse-function-in-python
-def egcd(a, b):
-    if a == 0:
-        return (b, 0, 1)
-    else:
-        g, y, x = egcd(b % a, a)
-        return (g, x - (b // a) * y, y)
-
-def modinv(a, m):
-    g, x, y = egcd(a, m)
-    if g != 1:
-        raise Exception('modular inverse does not exist')
-    else:
-        return x % m
-
-
 def main():
 
     lines = open_data("25.data")
 
     c_p, d_p = ints(lines)
 
-
-    # modinv(5764801, 20201227) = 7^n
-    print(round(math.log(modinv(5764801,  20201227))/math.log(7)))
-
-    # 5249543 = 7^x % 2020122
+    # d_p = 7^x % 20201227
+    #
+    # Calc solution via wolfram-alpha:
+    # https://www.wolframalpha.com/input/?i=5249543+%3D+%28%287%5Ex%29+mod+20201227%29
     x = 8419519
 
-    print(pow(14082811, x) % 20201227)
+    print(pow(c_p, x, 20201227))
 
 
 if __name__ == "__main__":
