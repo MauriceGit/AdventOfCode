1ca2587023543ae311a38782bf55c05584db63b0
mauricegit <github@tollmien.de>
2021-10-20 06:47:43 +0200

    all test cases work now.........

--- 2018/15.py
+++ 2018/15.py
@@ -23,55 +23,50 @@ def closest_target(orig_graph, units, current_index):
         except Exception as e:
             pass
 
-    all_paths.sort(key=lambda x: (len(x), x[0][0], x[0][1]))
+    all_paths.sort(key=lambda x: (len(x), x[0][1], x[0][0]))
 
     # first node is the current node, last node is the target node!
     # leave the target node in so we know where enemies are
     return [] if len(all_paths) == 0 else all_paths[0][1:][:-1]
 
+def length2(p):
+    return abs(p[0]) + abs(p[1])
 
 def get_inrange_target(p, units, team):
     t_index = -1
     for i, u in enumerate(units):
-        if u[2] != team and length(sub(u[0], p)) == 1 and (t_index == -1 or u[1] < units[t_index][1]):
+        if u[2] != team and length2(sub(u[0], p)) == 1 and (t_index == -1 or u[1] < units[t_index][1]):
             t_index = i
     return t_index
 
 
 def run_round(graph, units):
 
-
-    action = False
-
     i = 0
     while i < len(units):
         u = units[i]
 
+        if all(x[2] == units[0][2] for x in units):
+            return False
+
         path = closest_target(graph, units, i)
         if len(path) > 0:
             units[i][0] = path[0]
-            action = True
 
         target_index = get_inrange_target(units[i][0], units, u[2])
         if target_index != -1:
-            action = True
             units[target_index][1] -= 3
             if units[target_index][1] <= 0:
                 del units[target_index]
                 if target_index < i:
-                    i -= 1
+                    continue
         i += 1
 
-    return action
+    return True
 
 
 
-
-
-def main():
-
-    lines = open_data("15.data")
-
+def run(lines):
     graph = nx.Graph()
     # (pos, health, team)
     units = []
@@ -93,7 +88,16 @@ def main():
 
     print(units)
     print(rounds)
-    print((rounds-1) * sum(x[1] for x in units))
+    print((rounds) * sum(x[1] for x in units))
+
+
+def main():
+
+    groups = open_data_groups("15.data")
+
+    for g in groups:
+        run(g)
+        print()
 
 
 
