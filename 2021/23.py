d6a34f57035467e28395059c148b691c315e5e16
mauricegit <github@tollmien.de>
2021-12-23 15:59:21 +0100

    finished day 23. Not good, but done.

--- 2021/23.py
+++ 2021/23.py
@@ -5,6 +5,24 @@ sys.path.append('../General')
 from utility import *
 
 
+def pp(occupied, final_pos, hallway):
+    field = dict()
+    for x in range(-1, 12):
+        for y in range(-1, 6):
+            field[(x,y)] = "#"
+    for x in range(10):
+        field[(x, 0)] = "."
+    for hw in hallway:
+        field[hw] = "."
+    for ps in final_pos.values():
+        for p in ps:
+            field[p] = "."
+    for p, t in occupied.items():
+        if occupied[p] != "":
+            field[p] = t
+    draw(field, print_directly=True)
+
+
 def horizontal(p0, p1):
     positions = []
     x,y = p0
@@ -13,6 +31,7 @@ def horizontal(p0, p1):
         positions.append((x,y))
     return positions
 
+
 def vertical(p0, p1):
     positions = []
     x, y = p0
@@ -20,7 +39,6 @@ def vertical(p0, p1):
         y -= 1
         positions.append((x,y))
     return positions
-
 
 
 def all_steps_unoccupied(occupied, amph_type, p0, p1):
@@ -39,7 +57,6 @@ def all_steps_unoccupied(occupied, amph_type, p0, p1):
 
 
 def anything_clear_below(final_positions, occupied, p1):
-
     for p in final_positions:
         if p[1] > p1[1] and occupied[p] == "":
             return True
@@ -47,9 +64,6 @@ def anything_clear_below(final_positions, occupied, p1):
 
 
 def path_clear(occupied, final_pos, amph_type, p0, p1):
-
-    if p0 in final_pos[amph_type]:
-        return False
 
     # stays in hallway
     # tries to skip hallway
@@ -62,14 +76,8 @@ def path_clear(occupied, final_pos, amph_type, p0, p1):
 
     final_positions = final_pos[amph_type]
     ok = all([occupied[p] in ["", amph_type] for p in final_positions])
-
     if p1[1] > 0 and not ok:
         return False
-
-    # hallway to room
-    #if p1[1] == 1 and occupied[(p1[0],2)] == "":
-    #    # stops at first room pos but second one is empty as well!
-    #    return False
 
     # tries to stop at the upper-most position
     if p1[1] > 0 and anything_clear_below(final_positions, occupied, p1):
@@ -79,23 +87,19 @@ def path_clear(occupied, final_pos, amph_type, p0, p1):
     return all_steps_unoccupied(occupied, amph_type, p0, p1)
 
 
-# make generator!
 def get_all_moves(amphipods, occupied, final_pos, hallway, energie, steps):
     all_moves = []
     for amph_type, amph_l in amphipods.items():
         for amph in amph_l:
             for hp in hallway:
                 ok = path_clear(occupied, final_pos, amph_type, amph, hp)
-                #print("{} -> {}: {}", amph, hp)
                 if ok:
-                    #yield (steps[(amph, hp)]*energie[amph_type], amph_type, amph, hp)
                     all_moves.append((steps[(amph, hp)]*energie[amph_type], amph_type, amph, hp))
             for fp in final_pos[amph_type]:
                 if path_clear(occupied, final_pos, amph_type, amph, fp):
-                    #yield (steps[(amph, fp)]*energie[amph_type], amph_type, amph, fp)
-                    #print()
                     all_moves.append((steps[(amph, fp)]*energie[amph_type], amph_type, amph, fp))
     return all_moves
+
 
 def all_final(amphipods, final_pos):
     for amph_type, amph_l in amphipods.items():
@@ -109,23 +113,15 @@ def make_move(occupied, amphipods, t, pfrom, pto):
     occupied[pfrom] = ""
     occupied[pto] = t
     amphipods[t] = (amphipods[t] - set([pfrom])) | set([pto])
-    #amphipods[t].add(pto)
-    #amphipods[t].remove(pfrom)
-
-
-
-def state(occ, amphs):
-    return tuple(occ.items())
 
 
 def move(total_energie, path, _amphipods, _occupied, final_pos, hallway, energie, steps, level=0):
 
-
     # make a list of all possible moves room --> hallway and hallway --> final_room.
     moves = get_all_moves(_amphipods, _occupied, final_pos, hallway, energie, steps)
 
-    # [(used_energie, move, path)]
-    queue = [(0, move, []) for move in moves]
+    # [(used_energie, path)]
+    queue = [(0, [])]
     heapify(queue)
 
     visited = set()
@@ -135,112 +131,38 @@ def move(total_energie, path, _amphipods, _occupied, final_pos, hallway, energie
         occ = _occupied.copy()
         amphs = _amphipods.copy()
 
-        used_energie, move, path = heappop(queue)
-        #e, t, pfrom, pto = move
+        used_energie, path = heappop(queue)
 
         for m in path:
             make_move(occ, amphs, m[1], m[2], m[3])
 
         # check, if we're done!
         if all_final(amphs, final_pos):
-            return used_energie, path
+            return used_energie
 
-        s = state(occ, amphs)
-        if s in visited:
+        key = tuple(occ.items())
+        if key in visited:
             continue
-        visited.add(s)
+        visited.add(key)
 
         new_moves = get_all_moves(amphs, occ, final_pos, hallway, energie, steps)
         for m in new_moves:
-            heappush(queue, (used_energie+m[0], m, path + [m]))
-
+            heappush(queue, (used_energie+m[0], path + [m]))
 
     return 100000000, []
 
 
-
-
-def pp(occupied, final_pos, hallway):
-
-    field = dict()
-
-    for x in range(-1, 12):
-        for y in range(-1, 6):
-            field[(x,y)] = "#"
-
-    for x in range(10):
-        field[(x, 0)] = "."
-
-    for hw in hallway:
-        field[hw] = "."
-
-    for ps in final_pos.values():
-        for p in ps:
-            field[p] = "."
-
-    for p, t in occupied.items():
-        if occupied[p] != "":
-            field[p] = t
-
-    draw(field, print_directly=True)
-
-
-
-def main():
-
-    groups = open_data_groups("23.data")
-
-    # Part 1:
-    #############
-    #...........#
-    ###B#A#B#C###
-      #C#D#D#A#
-      #########
-
-    # Part 2:
-    #############
-    #...........#
-    ###B#A#B#C###
-      #D#C#B#A#
-      #D#B#A#C#
-      #C#D#D#A#
-      #########
-
-
-    # There are only exactly of two steps for each amphipod:
-    #   pos --> hallway --> final_pos
-
-
-    lines = groups[0]
+def doyourthing(amphipods, final_pos):
 
     hallway = [(0,0), (1,0), (3,0), (5,0), (7,0), (9,0), (10,0)]
-    amphipods = defaultdict(set)
-
-    for y,l in enumerate(lines):
-        for x,c in enumerate(l):
-            if c in "ABCD":
-                amphipods[c].add((x-1, y-1))
-
-    final_pos = {
-        "A": [(2, y+1) for y in range(len(lines)-3)],
-        "B": [(4, y+1) for y in range(len(lines)-3)],
-        "C": [(6, y+1) for y in range(len(lines)-3)],
-        "D": [(8, y+1) for y in range(len(lines)-3)]
-    }
-
-
-
     occupied = defaultdict(lambda: "")
     energie = {"A": 1, "B": 10, "C": 100, "D": 1000}
-
-    #return
 
     for t, ps in amphipods.items():
         for p in ps:
             occupied[p] = t
 
-    pp(occupied, final_pos, hallway)
-    #return
+    #pp(occupied, final_pos, hallway)
 
     steps = dict()
     for hp in hallway:
@@ -249,13 +171,29 @@ def main():
                 steps[(p, hp)] = manhatten_dist(p, hp)
                 steps[(hp, p)] = manhatten_dist(p, hp)
 
-    score, moves = move(0, [], amphipods, occupied, final_pos, hallway, energie, steps)
+    return move(0, [], amphipods, occupied, final_pos, hallway, energie, steps)
 
-    for m in moves:
-        e, t, pfrom, pto = m
-        make_move(occupied, amphipods, t, pfrom, pto)
-        pp(occupied, final_pos, hallway)
-    print(score)
+
+def main():
+
+    groups = open_data_groups("23.data")
+
+    for start_field in groups:
+
+        amphipods = defaultdict(set)
+        for y,l in enumerate(start_field):
+            for x,c in enumerate(l):
+                if c in "ABCD":
+                    amphipods[c].add((x-1, y-1))
+
+        final_pos = {
+            "A": [(2, y+1) for y in range(len(start_field)-3)],
+            "B": [(4, y+1) for y in range(len(start_field)-3)],
+            "C": [(6, y+1) for y in range(len(start_field)-3)],
+            "D": [(8, y+1) for y in range(len(start_field)-3)]
+        }
+
+        print(doyourthing(amphipods, final_pos))
 
 
 if __name__ == "__main__":
@@ -263,4 +201,4 @@ if __name__ == "__main__":
 
 # year 2021
 # solution for 23.01: 14510
-# solution for 23.02: ?
+# solution for 23.02: 49180
