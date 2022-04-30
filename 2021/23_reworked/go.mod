a86d5e546e66e1cdd3f037c2e4afe64f7496030b
mauricegit <github@tollmien.de>
2022-04-30 09:16:19 +0200

    string to []byte for another performance boost. Under 200ms for both parts now.

--- 2021/23_reworked/go.mod
+++ 2021/23_reworked/go.mod
@@ -1,3 +1,5 @@
 module 23_reworked
 
 go 1.18
+
+require github.com/pkg/profile v1.6.0
--- 2021/23_reworked/main.go
+++ 2021/23_reworked/main.go
@@ -5,24 +5,31 @@ import (
 	"container/heap"
 	"fmt"
 	"time"
+
+	"github.com/pkg/profile"
 )
 
 const (
 	HALL_LENGTH = 11
 )
 
-var gFinalRooms string
+var gFinalRooms [16]byte
 var gRoomHeight int
+var gRoomsArrayLength int
 var gRoomX = []int{2, 4, 6, 8}
-var gCheckedNodes int
+var gNextSteps [28]RoomState
 
-var gStepCost = map[rune]int{'a': 1, 'b': 10, 'c': 100, 'd': 1000}
+var gStepCost = map[byte]int{'a': 1, 'b': 10, 'c': 100, 'd': 1000}
 
 type RoomState struct {
-	cost          int
-	heuristicCost int
-	hall          string
-	rooms         string
+	cost  int
+	hall  [HALL_LENGTH]byte
+	rooms [16]byte
+}
+
+type VisitedKey struct {
+	hall  [HALL_LENGTH]byte
+	rooms [16]byte
 }
 
 type PriorityQueue []RoomState
@@ -32,7 +39,7 @@ func (s PriorityQueue) Len() int {
 	return len(s)
 }
 func (s PriorityQueue) Less(i, j int) bool {
-	return s[i].cost+s[i].heuristicCost < s[j].cost+s[j].heuristicCost
+	return s[i].cost < s[j].cost
 }
 func (s PriorityQueue) Swap(i, j int) {
 	s[i], s[j] = s[j], s[i]
@@ -50,7 +57,7 @@ func (s *PriorityQueue) Pop() any {
 	return x
 }
 
-func isInFinalPosition(c rune, col, yPos int, rooms string) bool {
+func isInFinalPosition(c byte, col, yPos int, rooms [16]byte) bool {
 	// The final column is: a->0, b->1, c->2, d->3. So ascii-97.
 	if int(c)-97 != col {
 		return false
@@ -58,14 +65,14 @@ func isInFinalPosition(c rune, col, yPos int, rooms string) bool {
 
 	// If there is anything underneath the column that is not the same (correct) character, than the final position is not reached yet!
 	for p := yPos + 1; p < gRoomHeight; p++ {
-		if rune(rooms[col*gRoomHeight+p]) != c {
+		if byte(rooms[col*gRoomHeight+p]) != c {
 			return false
 		}
 	}
 	return true
 }
 
-func canMoveUp(col, yPos int, rooms string) bool {
+func canMoveUp(col, yPos int, rooms [16]byte) bool {
 	for p := 0; p < yPos; p++ {
 		if rooms[col*gRoomHeight+p] != ' ' {
 			return false
@@ -74,62 +81,31 @@ func canMoveUp(col, yPos int, rooms string) bool {
 	return true
 }
 
-// Calculates a rough heuristic of remaining costs based on the manhatten distance of remaining characters
-// Results: It explores ~36000 fewer nodes but takes ~50ms longer because calculating the heuristic takes longer than just
-// trying some more nodes...
-func heuristic(s RoomState) (cost int) {
-
-	return
-
-	for i, c := range s.rooms {
-		if c != ' ' && int(c)-97 != int(i/gRoomHeight) {
-			cost += (i % gRoomHeight)
-		}
-	}
-	for _, c := range s.hall {
-		if c != ' ' && c != 'x' {
-			dstCol := int(c) - 97
-			dstHallPos := dstCol*2 + 2
-
-			tmp := dstHallPos - dstCol
-			if tmp < 0 {
-				tmp *= -1
-			}
-			cost += tmp
-		}
-	}
-
-	return
-}
-
-func moveRoomToHall(rPos, hPos, roomSteps, hallSteps int, c rune, s RoomState) RoomState {
-	// TODO: don't use strings at all and switch to []rune/[]byte in general for performance reasons, to avoid the conversions?
-	tmpRooms := []rune(s.rooms)
-	tmpRooms[rPos] = ' '
-
-	tmpHall := []rune(s.hall)
-	tmpHall[hPos] = c
-
+func moveRoomToHall(rPos, hPos, roomSteps, hallSteps int, c byte, s RoomState) RoomState {
 	cost := (hallSteps + roomSteps + 1) * gStepCost[c]
 
-	return RoomState{s.cost + cost, heuristic(s), string(tmpHall), string(tmpRooms)}
+	tmpS := s
+	tmpS.rooms[rPos] = ' '
+	tmpS.hall[hPos] = c
+	tmpS.cost += cost
+
+	return tmpS
 }
 
-func moveHallToRoom(hPos, rPos, roomSteps, hallSteps int, c rune, s RoomState) RoomState {
-	tmpHall := []rune(s.hall)
-	tmpHall[hPos] = ' '
-
-	tmpRooms := []rune(s.rooms)
-	tmpRooms[rPos] = c
-
+func moveHallToRoom(hPos, rPos, roomSteps, hallSteps int, c byte, s RoomState) RoomState {
 	cost := (hallSteps + roomSteps + 1) * gStepCost[c]
 
-	return RoomState{s.cost + cost, heuristic(s), string(tmpHall), string(tmpRooms)}
+	tmpS := s
+	tmpS.hall[hPos] = ' '
+	tmpS.rooms[rPos] = c
+	tmpS.cost += cost
+
+	return tmpS
 }
 
 // Does not check the start position and does not check the end position!
 // Both are OK, as you can't stand above a room and it should not check the start position anyway!
-func checkHallUntil(start, end, incr int, hall string) bool {
+func checkHallUntil(start, end, incr int, hall [HALL_LENGTH]byte) bool {
 	// Check, if the hall is empty until dstHallPos
 	for p := start + incr; p != end; p += incr {
 		h := hall[p]
@@ -143,11 +119,11 @@ func checkHallUntil(start, end, incr int, hall string) bool {
 	return true
 }
 
-func roomPlacementPossible(col int, c rune, rooms string) (int, bool) {
+func roomPlacementPossible(col int, c byte, rooms [16]byte) (int, bool) {
 
 	lastIndex := 0
 	for i := 0; i < gRoomHeight; i++ {
-		p := rune(rooms[col*gRoomHeight+i])
+		p := byte(rooms[col*gRoomHeight+i])
 
 		if p == ' ' {
 			lastIndex = i
@@ -161,14 +137,20 @@ func roomPlacementPossible(col int, c rune, rooms string) (int, bool) {
 	return lastIndex, true
 }
 
-func calcNextSteps(s RoomState) []RoomState {
+func calcNextSteps(s RoomState) int {
 
-	var nextSteps []RoomState
+	nextStepsCount := 0
+	// 4*7 is the theoretical maximum of next possible steps.
+	// That is: All the 7 hall places are empty and each of the 4 rooms tries all different hall placements.
+	//var nextSteps [28]RoomState
 
 	// Move from room to hall
 	for i, c := range s.rooms {
 		if c == ' ' {
 			continue
+		}
+		if i >= gRoomsArrayLength {
+			break
 		}
 
 		yPos := i % gRoomHeight
@@ -194,7 +176,8 @@ func calcNextSteps(s RoomState) []RoomState {
 			if h != ' ' {
 				break
 			}
-			nextSteps = append(nextSteps, moveRoomToHall(i, p, yPos, hallCol-p, c, s))
+			gNextSteps[nextStepsCount] = moveRoomToHall(i, p, yPos, hallCol-p, c, s)
+			nextStepsCount++
 		}
 		// Check open positions to the right and break if one is not empty
 		for p := hallCol + 1; p < HALL_LENGTH; p++ {
@@ -205,7 +188,8 @@ func calcNextSteps(s RoomState) []RoomState {
 			if h != ' ' {
 				break
 			}
-			nextSteps = append(nextSteps, moveRoomToHall(i, p, yPos, p-hallCol, c, s))
+			gNextSteps[nextStepsCount] = moveRoomToHall(i, p, yPos, p-hallCol, c, s)
+			nextStepsCount++
 		}
 	}
 
@@ -230,14 +214,14 @@ func calcNextSteps(s RoomState) []RoomState {
 
 			// Check, if there are only correct things in the final room!
 			if index, ok := roomPlacementPossible(dstCol, c, s.rooms); ok {
-
-				nextSteps = append(nextSteps, moveHallToRoom(i, dstCol*gRoomHeight+index, index, (dstHallPos-i)*hallDir, c, s))
+				gNextSteps[nextStepsCount] = moveHallToRoom(i, dstCol*gRoomHeight+index, index, (dstHallPos-i)*hallDir, c, s)
+				nextStepsCount++
 			}
 		}
 
 	}
 
-	return nextSteps
+	return nextStepsCount
 }
 
 func aStar(state RoomState) int {
@@ -246,27 +230,26 @@ func aStar(state RoomState) int {
 
 	heap.Push(priorityQueue, state)
 
-	visited := make(map[string]bool)
+	visited := make(map[VisitedKey]bool)
 
 	for priorityQueue.Len() > 0 {
 		var s = heap.Pop(priorityQueue).(RoomState)
-
-		gCheckedNodes++
 
 		if s.rooms == gFinalRooms {
 			return s.cost
 		}
 
-		if _, ok := visited[s.hall+s.rooms]; ok {
+		key := VisitedKey{s.hall, s.rooms}
+		if _, ok := visited[key]; ok {
 			continue
 		}
-		visited[s.hall+s.rooms] = true
+		visited[key] = true
 
-		nextSteps := calcNextSteps(s)
-
-		for _, nextStep := range nextSteps {
-			heap.Push(priorityQueue, nextStep)
+		nextStepsCount := calcNextSteps(s)
+		for i := 0; i < nextStepsCount; i++ {
+			heap.Push(priorityQueue, gNextSteps[i])
 		}
+
 	}
 
 	return 0
@@ -279,28 +262,48 @@ func elapsed() func() {
 	}
 }
 
+func parseRoomsString(s string) (res [16]byte) {
+	for i, c := range s {
+		res[i] = byte(c)
+	}
+	return
+}
+func parseHallString(s string) (res [HALL_LENGTH]byte) {
+	for i, c := range s {
+		res[i] = byte(c)
+	}
+	return
+}
+
 func main() {
 
+	defer profile.Start(profile.ProfilePath(".")).Stop()
+
+	emptyHall := parseHallString("  x x x x  ")
 	parts := []struct {
-		finalRoom  string
-		roomHeight int
-		hall       string
-		rooms      string
-	}{{"aabbccdd", 2, "  x x x x  ", "bcadbdca"}, {"aaaabbbbccccdddd", 4, "  x x x x  ", "bddcacbdbbadcaca"}}
+		finalRoom        [16]byte
+		roomHeight       int
+		roomsArrayLength int
+		hall             [HALL_LENGTH]byte
+		rooms            [16]byte
+	}{
+		{parseRoomsString("aabbccdd"), 2, 8, emptyHall, parseRoomsString("bcadbdca")},
+		{parseRoomsString("aaaabbbbccccdddd"), 4, 16, emptyHall, parseRoomsString("bddcacbdbbadcaca")},
+	}
 
 	var timings [2]int64
 
 	for i, part := range parts {
 		gFinalRooms = part.finalRoom
 		gRoomHeight = part.roomHeight
+		gRoomsArrayLength = part.roomsArrayLength
 
 		start := time.Now()
-		score := aStar(RoomState{0, 0, part.hall, part.rooms})
+		score := aStar(RoomState{0, part.hall, part.rooms})
 		timings[i] = time.Since(start).Milliseconds()
 
 		fmt.Printf("Part %v: %v in %vms\n", i+1, score, timings[i])
 	}
 
 	fmt.Printf("Total runtime: %vms\n", timings[0]+timings[1])
-	fmt.Printf("Checked nodes: %v\n", gCheckedNodes)
 }
