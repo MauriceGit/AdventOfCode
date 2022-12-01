2426e804e614a69239840bcc77948a25d2f0f97b
mauricegit <github@tollmien.de>
2022-12-01 07:02:25 +0100

    finished day 01

--- 2022/01.py
+++ 2022/01.py
@@ -5,20 +5,17 @@ sys.path.append('../General')
 from utility import *
 
 
-
 def main():
 
-    lines = open_data("01.data")
+    counts = sorted(map(sum, map(ints, open_data_groups("01.data"))))
 
-
-
-
-
+    print(counts[-1])
+    print(sum(counts[-3:]))
 
 
 if __name__ == "__main__":
     main()
 
 # year 2022
-# solution for 01.01: ?
-# solution for 01.02: ?
+# solution for 01.01: 69626
+# solution for 01.02: 206780
