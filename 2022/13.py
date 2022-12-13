ca4d0ba938f6d0e7f416981cfe9c042c93ea017f
mauricegit <github@tollmien.de>
2022-12-13 12:22:25 +0100

    some fixes

--- 2022/13.py
+++ 2022/13.py
@@ -5,33 +5,30 @@ sys.path.append('../General')
 from utility import *
 
 
-def compare2(l, r):
+def compare(l, r):
+
     for i in range(max(len(l), len(r))):
         if i >= len(r):
             return False
         if i >= len(l):
             return True
 
-        if type(l[i]) == int and type(r[i]) == int:
-            if int(l[i]) > int(r[i]):
-                return False
-            if int(l[i]) < int(r[i]):
-                return True
-        elif type(l[i]) == list and type(r[i]) == list:
-            if (res := compare2(l[i], r[i])) != None:
-                return res
-        else:
-            if type(r[i]) == int:
-                if (res := compare2(l[i], (r[:i]+[[r[i]]]+r[i+1:])[i])) != None:
+        match l[i], r[i]:
+            case int(), int():
+                if l[i] > r[i]:
+                    return False
+                if l[i] < r[i]:
+                    return True
+            case list(), list():
+                if (res := compare(l[i], r[i])) != None:
                     return res
-            else:
-                if (res := compare2((l[:i]+[[l[i]]]+l[i+1:])[i], r[i])) != None:
+            case list(), int():
+                if (res := compare(l[i], (r[:i]+[[r[i]]]+r[i+1:])[i])) != None:
+                    return res
+            case int(), list():
+                if (res := compare((l[:i]+[[l[i]]]+l[i+1:])[i], r[i])) != None:
                     return res
     return None
-
-
-def compare(l, r):
-    return -1 if compare2(l, r) else 1
 
 
 def main():
@@ -42,14 +39,13 @@ def main():
     lines = []
     for i, g in enumerate(groups):
         lines.extend([loads(g[0]), loads(g[1])])
-        if compare2(lines[-2], lines[-1]):
-            s += i+1
+        s += (i+1)*compare(lines[-2], lines[-1])
     print(s)
 
     lines.append([[2]])
     lines.append([[6]])
 
-    lines.sort(key=cmp_to_key(compare))
+    lines.sort(key=cmp_to_key(lambda l, r: -1 if compare(l, r) else 1))
     print((lines.index([[2]])+1) * (lines.index([[6]])+1))
 
 
