82307a8f08f846e0bc9e94e5f8fa190778e7327f
mauricegit <github@tollmien.de>
2022-12-17 10:27:14 +0100

    Fixed small bug. Now able to calculate the correct value. Part1 done. Part2 to go

--- 2022/16.py
+++ 2022/16.py
@@ -9,29 +9,26 @@ State = recordtype("State", "score open_valves v minutes history")
 
 
 cache = dict()
-
 def try_valve2(valves, open_valves, current, minutes, score, history):
-
-    key = (open_valves, current, minutes)
-    if key in cache:
-        return cache[key]
 
     if minutes >= 30:
         return score, history
 
+    key = (score, current, minutes)
+    if key in cache:
+        return cache[key]
 
-    best = (0, "")
+    best = (score, history)
     for open_valve in [True, False]:
-        # open valve, if closed
         tmp_history = history
         tmp_open_valves = open_valves
         tmp_minutes = minutes
         tmp_score = score
 
         ok = not open_valve
-        if open_valve and valves[current].flow_rate > 0 and current not in tmp_open_valves:
+        if open_valve and valves[current].flow_rate > 0 and f"[{current}]" not in tmp_open_valves:
             tmp_history += f" [{current}]"
-            tmp_open_valves += current
+            tmp_open_valves += f"[{current}]"
             tmp_minutes += 1
             tmp_score += (30-tmp_minutes) * valves[current].flow_rate
             ok = True
@@ -42,7 +39,6 @@ def try_valve2(valves, open_valves, current, minutes, score, history):
                 if s > best[0]:
                     best = (s, p)
 
-    key = (open_valves, current, minutes)
     cache[key] = best
     return best
 
@@ -59,37 +55,15 @@ def main():
         v = l[1]
         flow_rate = ints(line)[0]
         vs = line.split(" to ")[1].replace(",", "").split(" ")[1:]
-        #print(f"{v} --> {valves}")
-        # (flow_rate, is_open, tunnels)
         valves[v] = Valve(flow_rate, False, vs)
 
 
-    #pressure_release = 0
-    #print(try_valve(valves, "AA", 1, pressure_release, 30, "AA "))
-
     print(try_valve2(valves, "", "AA", 0, 0, "AA"))
-
-    #pressure_release = []
-    #
-    #r = bfs(valves, 30)
-    #print(r)
-    #
-    #print(sum(r.pressure_release))
-
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
     main()
 
 # year 2022
-# solution for 16.01: ? < 3303, < 2412, not 2263
+# solution for 16.01: 2265
 # solution for 16.02: ?
