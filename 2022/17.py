bc45dc628063f9d531cce51a1f1369ee7941b50b
mauricegit <github@tollmien.de>
2022-12-17 18:10:17 +0100

    Part2 for day 17 is done. This is really fun :)

--- 2022/17.py
+++ 2022/17.py
@@ -33,6 +33,27 @@ def place_rock(field, pos, rock):
             if c == "#":
                 field[add(pos, (x,-y))] = "#"
 
+def run(field, jet, rocks, iterations):
+    jet_i = 0
+    for i in range(iterations):
+        rock = rocks[i%len(rocks)]
+        pos = (2, max(field.keys(), key=lambda x: x[1])[1]+len(rock)+3)
+
+        for j in itertools.count():
+            wind = jet[jet_i%len(jet)]
+            pos = move_left_right(field, pos, rock, wind)
+            jet_i += 1
+
+            pos, rock_placed = move_down(field, pos, rock)
+
+            if rock_placed:
+                # print jet_i%len(jet) here for analysis when everything repeats!
+                break
+
+        place_rock(field, pos, rock)
+
+    return max(field.keys(), key=lambda x: x[1])[1]
+
 def main():
 
     jet = open_data("17.data")[0]
@@ -49,34 +70,28 @@ def main():
     for i in range(7):
         field[(i,0)] = "#"
 
-    jet_i = 0
-    for i in range(2022):
-        rock = rocks[i%len(rocks)]
-        pos = (2, max(field.keys(), key=lambda x: x[1])[1]+len(rock)+3)
+    print(run(field.copy(), jet, rocks, 2022))
 
-        for j in itertools.count():
-            wind = jet[jet_i%len(jet)]
-            pos = move_left_right(field, pos, rock, wind)
-            jet_i += 1
+    # Initial offset is 800 repetitions (probably fewer but who cares).
+    # Then 1700 jet (input) indices repeat forever! (counted when a rock is finally placed!)
+    # The initial height at 800 repetitions is: 1230
+    # Then the height after each group is: 1230, 3884, 6538
+    # So the height grows 2654 each iteration
+    #
+    # That means, after subtracting 800 rounds, we have 999999999200 rounds remaining.
+    # So dividing this by 1700, we have 588235293 full block repetitions (*2654 height)
+    # After that, in the end, we have 1100 rounds remaining (999999999200%1700).
+    #
+    # The initial 800 rounds + 1100 rounds remaining create a tower of height: 2947
+    #
+    # So 588235293*2654 + 2947 == 1561176470569
 
-            pos, rock_placed = move_down(field, pos, rock)
-
-            if rock_placed:
-                break
-
-        place_rock(field, pos, rock)
-
-    print(max(field.keys(), key=lambda x: x[1])[1])
-
-
-
-
-
+    print(588235293*2654 + run(field.copy(), jet, rocks, 800+1100))
 
 
 if __name__ == "__main__":
     main()
 
 # year 2022
-# solution for 17.01: ?
-# solution for 17.02: ?
+# solution for 17.01: 3124
+# solution for 17.02: 1561176470569
