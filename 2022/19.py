c98dad42590cadc6eee3cf12f155ae0c8bf6a01b
mauricegit <github@tollmien.de>
2022-12-19 08:19:10 +0100

    does not seem to work for part2

--- 2022/19.py
+++ 2022/19.py
@@ -10,13 +10,12 @@ best_result = 0
 cache = dict()
 def run_blueprint(bp, ores, bots, minute):
 
-    if minute >= 24:
+    if minute >= 32:
         return ores[3]
 
     o = ores[0]<<18|ores[1]<<12|ores[2]<<6|ores[3]
     b = bots[0]<<18|bots[1]<<12|bots[2]<<6|bots[3]
     key = o<<30 | b<<6 | minute
-    #key = ores[3]<<30 | b<<6 | minute
     #key = (tuple(ores), tuple(bots), minute)
     if key in cache:
         return cache[key]
@@ -49,16 +48,13 @@ def run_blueprint(bp, ores, bots, minute):
     #    best = max(best, run_blueprint(bp, sub(ores, bp[bi]), add(bots, b), minute+1))
 
     best = ores[3]
-    tried_something = False
     # if we can build a geode robot, we only do that!
     if ores[2] >= bp[3][2] and ores[0] >= bp[3][0]:
         best = max(best, run_blueprint(bp, add(new_ores, sub(ores, bp[3])), add(bots, (0,0,0,1)), minute+1))
-        tried_something = True
     elif(ores[1] >= bp[2][1] and ores[0] >= bp[2][0]):
         best = max(best, run_blueprint(bp, add(new_ores, sub(ores, bp[2])), add(bots, (0,0,1,0)), minute+1))
-        tried_something = True
     else:
-        for bi, b in enumerate(bp[:2]):
+        for bi, b in reversed(list(enumerate(bp[:2]))):
             ok = True
             for i, ore in enumerate(b):
                 if ores[i] < ore:
@@ -67,12 +63,8 @@ def run_blueprint(bp, ores, bots, minute):
             if ok:
                 new_bot = tuple([int(i==bi) for i in range(4)])
                 best = max(best, run_blueprint(bp, add(new_ores, sub(ores, bp[bi])), add(bots, new_bot), minute+1))
-                tried_something = True
 
         best = max(best, run_blueprint(bp, add(new_ores, ores), bots, minute+1))
-
-
-
 
     cache[key] = best
     global best_result
@@ -99,7 +91,7 @@ def main():
 
 
     quality = 0
-    for i,bp in enumerate(blueprints):
+    for i,bp in enumerate(blueprints[1:]):
         r = run_blueprint(bp, [0,0,0,0], [1,0,0,0], 0)
         print(f"Blueprint {i+1} can create {r} geodes")
         quality += (i+1)*r
