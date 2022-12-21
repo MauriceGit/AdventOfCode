24d4e28250dffaaf2971f3d2b1bb9446255c5ceb
mauricegit <github@tollmien.de>
2022-12-21 16:17:08 +0100

    proper solution for both parts

--- 2022/21.py
+++ 2022/21.py
@@ -4,33 +4,56 @@ import sys
 sys.path.append('../General')
 from utility import *
 
+# for each equation, one part is always an int!
+def solve(monkeys, current, solution):
+
+    if current == "humn":
+        return solution
+
+    l, op, r = monkeys[current]
+
+    if type(l) == int:
+        match op:
+            case "+": return solve(monkeys, r, solution-l)
+            case "*": return solve(monkeys, r, solution//l)
+            case "/": return solve(monkeys, r, l//solution)
+            case "-": return solve(monkeys, r, l-solution)
+    if type(r) == int:
+        match op:
+            case "+": return solve(monkeys, l, solution-r)
+            case "*": return solve(monkeys, l, solution//r)
+            case "/": return solve(monkeys, l, solution*r)
+            case "-": return solve(monkeys, l, solution+r)
+
 
 def run(monkeys, current):
 
     if type(monkeys[current]) == int:
-        if current == "humn":
-            return monkeys[current], "x"
-        return monkeys[current], str(monkeys[current])
+        return monkeys[current], None if current == "humn" else monkeys[current]
 
-    l, op, r = monkeys[current]
+    l_m, op, r_m = monkeys[current]
 
-    d = {"+": operator.add, "*": operator.mul, "/": lambda x,y: x//y, "-": lambda x,y: x-y}
-    rl = run(monkeys, l)
-    rr = run(monkeys, r)
-    r = d[op](rl[0], rr[0])
+    d = {"+": operator.add, "*": operator.mul, "/": operator.floordiv, "-": operator.sub}
+    rl = run(monkeys, l_m)
+    rr = run(monkeys, r_m)
+    result = d[op](rl[0], rr[0])
+
+    if rr[1] != None and rl[1] == None:
+        monkeys[current][2] = rr[0]
+    if rl[1] != None and rr[1] == None:
+        monkeys[current][0] = rl[0]
+    if rl[1] != None and rr[1] != None:
+        monkeys[current] = result
 
     if current == "root":
-        return f"{rl[1]} = {rr[0]}"
+        return result, (rl[0] if rl[1] != None else rr[0], l_m if rl[1] == None else r_m)
 
-    return r, f"({rl[1]}) {op} ({rr[1]})"
-
-
+    return result, result if rl[1] != None and rr[1] != None else None
 
 
 def main():
 
     lines = open_data("21.data")
-
 
     monkeys = dict()
     for l in lines:
@@ -41,13 +64,14 @@ def main():
         else:
             monkeys[s[0]] = s[1].split(" ")
 
-    print(run(monkeys, "root"))
-
+    p1, p2 = run(monkeys, "root")
+    print(p1)
+    print(solve(monkeys, p2[1], p2[0]))
 
 
 if __name__ == "__main__":
     main()
 
 # year 2022
-# solution for 21.01: ?
-# solution for 21.02: ?
+# solution for 21.01: 364367103397416
+# solution for 21.02: 3782852515583
