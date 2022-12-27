81027473f4b629cb18368530c18f42b23cf899fd
mauricegit <github@tollmien.de>
2022-12-27 23:02:52 +0100

    simplify day 23

--- 2022/23.py
+++ 2022/23.py
@@ -6,64 +6,72 @@ from utility import *
 
 dirs = dict(zip(["E", "W", "S", "N", "SE", "NW", "SW", "NE"], dir_list_8()))
 
+def calc_move(elves, elve, rot):
+
+    ok = lambda keys: all(add(elve, dirs[k]) not in elves for k in keys)
+
+    for i in range(4):
+        match (i+rot)%4:
+            case 0:
+                if ok(["N", "NE", "NW"]):
+                    return add(elve, dirs["N"])
+            case 1:
+                if ok(["S", "SE", "SW"]):
+                    return add(elve, dirs["S"])
+            case 2:
+                if ok(["W", "NW", "SW"]):
+                    return add(elve, dirs["W"])
+            case 3:
+                if ok(["E", "NE", "SE"]):
+                    return add(elve, dirs["E"])
+    return None
+
+
 def propose_move(elves, elve, i):
-    if len({add(elve, dirs[d]) for d in dirs.keys()} & elves) == 0:
+    if all(add(elve, k) not in elves for k in dirs.values()):
         return None
 
-    steps = deque([None, None, None, None])
-    if len(elves & {add(elve, dirs["N"]), add(elve, dirs["NE"]), add(elve, dirs["NW"])}) == 0:
-        steps[0] = add(elve, dirs["N"])
-    if len(elves & {add(elve, dirs["S"]), add(elve, dirs["SE"]), add(elve, dirs["SW"])}) == 0:
-        steps[1] = add(elve, dirs["S"])
-    if len(elves & {add(elve, dirs["W"]), add(elve, dirs["NW"]), add(elve, dirs["SW"])}) == 0:
-        steps[2] = add(elve, dirs["W"])
-    if len(elves & {add(elve, dirs["E"]), add(elve, dirs["NE"]), add(elve, dirs["SE"])}) == 0:
-        steps[3] = add(elve, dirs["E"])
-    steps.rotate(-i)
-    for s in steps:
-        if s != None:
-            return s
-    return None
+    return calc_move(elves, elve, i)
 
 def next_round(elves, i):
     proposed = dict()
     spots = defaultdict(int)
 
     # First halve round
-    for e in elves:
+    for e in elves.keys():
         p = propose_move(elves, e, i)
         proposed[e] = p if p is not None else e
         spots[proposed[e]] += 1
 
-    new_elves = elves.copy()
     someone_moved = False
-    for e in elves:
+    for e in list(elves.keys()):
         if spots[proposed[e]] == 1:
-            new_elves.remove(e)
-            new_elves.add(proposed[e])
+            del elves[e]
+            elves[proposed[e]] = "#"
             someone_moved = someone_moved or proposed[e] != e
 
-    return new_elves, not someone_moved
+    return elves, not someone_moved
 
 
 def main():
 
     lines = open_data("23.data")
 
-    elves = set()
+    elves = dict()
     for y,l in enumerate(lines):
         for x,c in enumerate(l):
             if c == "#":
-                elves.add((x,y))
+                elves[(x,y)] = c
+
 
     for i in itertools.count():
         elves, done = next_round(elves, i)
 
         if i == 9:
-            min_x = min(elves, key=lambda x: x[0])[0]
-            max_x = max(elves, key=lambda x: x[0])[0]+1
-            min_y = min(elves, key=lambda x: x[1])[1]
-            max_y = max(elves, key=lambda x: x[1])[1]+1
+            min_x = min(elves.keys(), key=lambda x: x[0])[0]
+            max_x = max(elves.keys(), key=lambda x: x[0])[0]+1
+            min_y = min(elves.keys(), key=lambda x: x[1])[1]
+            max_y = max(elves.keys(), key=lambda x: x[1])[1]+1
             print((max_x-min_x)*(max_y-min_y) - len(elves))
         if done:
             print(i+1)
