a193ae01eb8ffa4a3d8d01313c3f419566c582cb
mauricegit <github@tollmien.de>
2023-01-04 08:32:21 +0100

    in the middle of a restructuring

--- 2022/20_go/day20.go
+++ 2022/20_go/day20.go
@@ -27,6 +27,13 @@ func newLinkedList() LinkedList {
 	return ll
 }
 
+func newLinkedList(size int) LinkedList {
+	var ll LinkedList
+	ll.nextFree = -1
+	ll.backing = make([]Node, size)
+	return ll
+}
+
 func (ll *LinkedList) RotateLeft(n int) {
 	if n < 0 {
 		n = ll.size - ((-n) % ll.size)
@@ -111,41 +118,84 @@ func (ll LinkedList) String() string {
 	return s
 }
 
-func mix(numbers LinkedList, origNumbers []int, numberMapping map[int]int) LinkedList {
+func findBucket(buckets []LinkedList, bi, offset, element int) (int, int) {
+	// count indices to get out of this bucket (and return early, if the element stays in this bucket!)
+
+	// we know, that element is in buckets[bi]!
+	count := 0
+	node := buckets[bi].start
+	for buckets[bi].backing[node].v != element {
+		node = buckets[bi].backing[node].next
+		count++
+	}
+
+	// element will be inserted into the same bucket again
+	if offset < (buckets[bi].size-1)-count {
+
+	}
+
+	// offset whole buckets
+
+	// iterate target bucket to find exact insertion position
+
+	// insert number at target bucket
+
+	// remove number from source bucket
+
+	// update nToBucket reference
+
+}
+
+func mix(buckets []LinkedList, origNumbers []int, numberMapping map[int]int, nToBucket *map[int]int) []LinkedList {
+
+	// for _, nn := range origNumbers {
+	// 	i := numbers.Index(nn)
+	// 	numbers.RotateLeft(i)
+	// 	n := numbers.PopLeft()
+	// 	numbers.RotateLeft(numberMapping[nn])
+	// 	numbers.AppendLeft(n)
+	// }
 
 	for _, nn := range origNumbers {
-		i := numbers.Index(nn)
-		numbers.RotateLeft(i)
-		n := numbers.PopLeft()
-		numbers.RotateLeft(numberMapping[nn])
-		numbers.AppendLeft(n)
+		bi := nToBucket[nn]
+		i := buckets[bi].Index(nn)
+		nextBi := findBucket(buckets, i)
 	}
 
 	return numbers
 }
 
 func getSolution(numbers LinkedList, num0 int, numberMapping map[int]int) int {
-	i := numbers.Index(num0)
-	numbers.RotateLeft(i)
-	numbers.RotateLeft(1000)
-	nn := numberMapping[numbers.backing[numbers.start].v]
-	numbers.RotateLeft(1000)
-	nn += numberMapping[numbers.backing[numbers.start].v]
-	numbers.RotateLeft(1000)
-	nn += numberMapping[numbers.backing[numbers.start].v]
-	return nn
+	// i := numbers.Index(num0)
+	// numbers.RotateLeft(i)
+	// numbers.RotateLeft(1000)
+	// nn := numberMapping[numbers.backing[numbers.start].v]
+	// numbers.RotateLeft(1000)
+	// nn += numberMapping[numbers.backing[numbers.start].v]
+	// numbers.RotateLeft(1000)
+	// nn += numberMapping[numbers.backing[numbers.start].v]
+
+	return 0
 }
 
 func main() {
 
 	//defer profile.Start(profile.ProfilePath(".")).Stop()
 
-	numbers := newLinkedList()
-	numbers2 := newLinkedList()
+	//numbers := newLinkedList()
+	//numbers2 := newLinkedList()
+
+	bucketSize := 10
+	var buckets []LinkedList
+
+	buckets = append(buckets, newLinkedList(bucketSize))
+
 	numberMapping := make(map[int]int)
 	numberMapping2 := make(map[int]int)
 	var origNumbers []int
 	var num0 int
+
+	nToBucket := make(map[int]int)
 
 	if aoc, ok := InitAOC(); ok {
 		for i, v := range aoc.GetLines() {
@@ -153,8 +203,12 @@ func main() {
 			conv, _ = strconv.Atoi(v)
 			numberMapping[i] = conv
 			numberMapping2[i] = conv * 811589153
-			numbers.Append(i)
-			numbers2.Append(i)
+			//numbers.Append(i)
+			//numbers2.Append(i)
+
+			buckets[len(buckets)-1].Append(i)
+			nToBucket[i] = len(buckets) - 1
+
 			origNumbers = append(origNumbers, i)
 			if conv == 0 {
 				num0 = i
@@ -162,9 +216,10 @@ func main() {
 		}
 	}
 
-	numbers = mix(numbers, origNumbers, numberMapping)
+	numbers = mix(numbers, origNumbers, numberMapping, &nToBucket)
 	fmt.Println(getSolution(numbers, num0, numberMapping))
 
+	return
 	for i := 0; i < 10; i++ {
 		numbers2 = mix(numbers2, origNumbers, numberMapping2)
 	}
