3ea766319bb0e49da9743b2afd1c59e3e93fd5cc
mauricegit <github@tollmien.de>
2023-12-10 12:33:03 +0100

    clean up day 10

--- 2023/10.py
+++ 2023/10.py
@@ -4,14 +4,12 @@ import sys
 sys.path.append('../General')
 from utility import *
 
-def connect(f, max_p, p, new_p):
-    if 0 <= new_p[0] <= max_p[0] and 0 <= new_p[1] <= max_p[1]:
-        f[p].append(new_p)
+State = recordtype("State", "f count path")
 
 # line to the left. Returns all encountered symbols
-def calc_line_count(path, f, p):
+def is_inside_path(path, f, p):
     if p in path:
-        return 0
+        return False
 
     count = 0
     start_from_up = None
@@ -29,97 +27,26 @@ def calc_line_count(path, f, p):
             if f[pp] == "7" and not start_from_up:
                 count += 1
 
-    return count
+    return count%2 != 0
 
-def main():
+def get_starting_positions(symbols, start_p):
+    if symbols[add(start_p, (0,-1))] in "|7F":
+        yield add(start_p, (0,-1))
+    if symbols[add(start_p, (0,1))] in "|LJ":
+        yield add(start_p, (0, 1))
+    if symbols[add(start_p, (-1,0))] in "-LF":
+        yield add(start_p, (-1,0))
+    if symbols[add(start_p, (1,0))] in "-7J":
+        yield add(start_p, ( 1,0))
 
-    lines = open_data("10.data")
-    max_p = (len(lines[0])-1, len(lines)-1)
-    start_p = None
-
-    f = defaultdict(list)
-    symbols = dict()
-    for y,line in enumerate(lines):
-        for x,c in enumerate(line):
-            symbols[(x,y)] = c
-            match c:
-                case "|":
-                    connect(f, max_p, (x,y), (x,y-1))
-                    connect(f, max_p, (x,y), (x,y+1))
-                case "-":
-                    connect(f, max_p, (x,y), (x-1,y))
-                    connect(f, max_p, (x,y), (x+1,y))
-                case "L":
-                    connect(f, max_p, (x,y), (x,y-1))
-                    connect(f, max_p, (x,y), (x+1,y))
-                case "J":
-                    connect(f, max_p, (x,y), (x,y-1))
-                    connect(f, max_p, (x,y), (x-1,y))
-                case "7":
-                    connect(f, max_p, (x,y), (x-1,y))
-                    connect(f, max_p, (x,y), (x,y+1))
-                case "F":
-                    connect(f, max_p, (x,y), (x+1,y))
-                    connect(f, max_p, (x,y), (x,y+1))
-                case "S":
-                    start_p = (x,y)
-
-
-    start_positions = []
-    if start_p in f[add(start_p, (0,-1))]:
-        start_positions.append(add(start_p, (0,-1)))
-    if start_p in f[add(start_p, (0, 1))]:
-        start_positions.append(add(start_p, (0, 1)))
-    if start_p in f[add(start_p, (-1,0))]:
-        start_positions.append(add(start_p, (-1,0)))
-    if start_p in f[add(start_p, ( 1,0))]:
-        start_positions.append(add(start_p, ( 1,0)))
-
-
-    State = recordtype("State", "f count path")
-
-    def get_neighbors(state, pos):
-        #match state.f[pos]:
-        #    case "|": return [add(pos,(0,-1)), add(pos,(0,1))]
-        #    case "-": return [add(pos,(-1,0)), add(pos,(1,0))]
-        #    case "L": return [add(pos,(0,-1)), add(pos,(1,0))]
-        #    case "J": return [add(pos,(0,-1)), add(pos,(-1,0))]
-        #    case "7": return [add(pos,(-1,0)), add(pos,(0,1))]
-        #    case "F": return [add(pos,(1,0)), add(pos,(0,1))]
-        #return []
-        return state.f[pos]
-
-
-    def visit(state, pos, dist, path):
-        #if state.f[pos] == "S":
-        if start_p in state.f[pos]:
-            state.count = max(state.count, dist)
-            if dist > 1:
-                state.path = path
-
-        return True
-
-    path = []
-    max_length = 0
-    for p in start_positions:
-        state = State(f, 0, [])
-        dijkstra(p, get_neighbors, state=state, visit=visit, use_path=True)
-        # +2 because we miss the first and the last step!
-        max_length = max(max_length, state.count+2)
-        if len(state.path) > len(path):
-            path = state.path
-
-    print(max_length//2)
-
-
-    # make sure to substitute the original "S" with the correct pipe symbol!
+# replace the 'S' symbol with the correct pipe symbol
+def fix_s_symbol(symbols, path, start_p):
     ps, pt = path[0], path[-1]
     pp = list(sorted([ps, pt], key=itemgetter(1)))
     if abs(ps[0]-pt[0]) == 2:
         symbols[start_p] = "-"
     if abs(ps[1]-pt[1]) == 2:
         symbols[start_p] = "|"
-
     if add(start_p, (0,-1)) == pp[0]:
         if add(start_p, (1,0)) == pp[1]:
             symbols[start_p] = "L"
@@ -132,8 +59,48 @@ def main():
             symbols[start_p] = "7"
 
 
+def main():
+
+    lines = open_data("10.data")
+
+    symbols = {(x,y): c for y,line in enumerate(lines) for x,c in enumerate(line)}
+    start_p = lfilter(lambda x: x[1] == "S", symbols.items())[0][0]
+
+    def get_neighbors(state, pos):
+        if pos not in state.f:
+            return []
+        match state.f[pos]:
+            case "|": return [add(pos,(0,-1)), add(pos,(0,1))]
+            case "-": return [add(pos,(-1,0)), add(pos,(1,0))]
+            case "L": return [add(pos,(0,-1)), add(pos,(1,0))]
+            case "J": return [add(pos,(0,-1)), add(pos,(-1,0))]
+            case "7": return [add(pos,(-1,0)), add(pos,(0,1))]
+            case "F": return [add(pos,(1,0)), add(pos,(0,1))]
+        return []
+
+    def visit(state, pos, dist, path):
+        if start_p in get_neighbors(state, pos):
+            state.count = max(state.count, dist)
+            if dist > 1:
+                state.path = path
+        return True
+
+    path = []
+    max_length = 0
+    for p in get_starting_positions(symbols, start_p):
+        state = State(symbols, 0, [])
+        dijkstra(p, get_neighbors, state=state, visit=visit, use_path=True)
+        # +2 because we miss the first and the last step!
+        max_length = max(max_length, state.count+2)
+        if len(state.path) > len(path):
+            path = state.path
+    print(max_length//2)
+
+    # make sure to substitute the original "S" with the correct pipe symbol!
+    fix_s_symbol(symbols, path, start_p)
+
     path = set(path+[start_p])
-    print(sum(calc_line_count(path, symbols, k)%2 != 0 for k in symbols.keys()))
+    print(sum(is_inside_path(path, symbols, k) for k in symbols.keys()))
 
 
 if __name__ == "__main__":
