27814bada69f9cdee1df9d6c2d42fc27a950210a
mauricegit <github@tollmien.de>
2023-12-27 09:53:43 +0100

    tidy day 21

--- 2023/21.py
+++ 2023/21.py
@@ -4,28 +4,9 @@ import sys
 sys.path.append('../General')
 from utility import *
 
-def calc_step_area_complete(start_pos, f, f_count, w, h, max_step_count):
-    State = recordtype("State", "field count")
-
-    def get_neighbors(state, pos):
-        for d in dir_list_4():
-            pos2 = add(pos, d)
-            pos2 = (pos2[0]%w, pos2[1]%h)
-            if state.field[pos2] != "#":
-                yield add(pos, d)
-
-    def visit(state, pos, dist, path):
-        if dist%2 == max_step_count%2:
-            pos2 = (pos[0]%w, pos[1]%h)
-            state.count[pos2] += 1
-        return dist <= max_step_count
-
-    state = State(f, f_count)
-    dijkstra(start_pos, get_neighbors, state=state, visit=visit)
-    return sum(f_count.values())
+State = recordtype("State", "field count")
 
 def calc_step_area(start_pos, f, f_count, w, h, max_step_count, even=True):
-    State = recordtype("State", "field count")
 
     def get_neighbors(state, pos):
         for d in dir_list_4():
@@ -37,9 +18,9 @@ def calc_step_area(start_pos, f, f_count, w, h, max_step_count, even=True):
             state.count[pos] = 1
         return dist <= max_step_count
 
-
     state = State(f, f_count)
     if type(start_pos) == list:
+        # fill the same field from different starting points
         for pos in start_pos:
             dijkstra(pos, get_neighbors, state=state, visit=visit)
     else:
@@ -65,23 +46,24 @@ def main():
     w = len(lines[0])
     h = len(lines)
 
-    print(calc_step_area_complete(pos, f, f_count.copy(), w, h, 64))
+    def calc_area(start, count, even):
+        return calc_step_area(start, f, f_count.copy(), w, h, count, even=even)
+
+    print(calc_area(pos, 64, True))
 
     original_steps = 26501365
     length = w
 
-    def calc_area(start, count, even):
-        return calc_step_area(start, f, f_count.copy(), w, h, count, even=even)
-
     steps = original_steps - (length//2)
     # The field count also starts with 'odd'
-    odd  = calc_step_area(pos, f, f_count.copy(), w, h, 10000000000, even=False)
-    even = calc_step_area(pos, f, f_count.copy(), w, h, 10000000000, even=True)
+    odd  = calc_area(pos, 10000000000, False)
+    even = calc_area(pos, 10000000000, True)
 
     field_cycles = steps//length
     remaining_steps = steps%length
 
     start_even = original_steps%2 == 0
+
     def base_coverage(step):
         count = odd
         while step > 0:
@@ -94,39 +76,29 @@ def main():
     is_even = field_cycles%2 != 0
 
     just_outside = 0
-    # bottom left
+    # bottom left, bottom right, top right, top left
     just_outside += calc_area((0,h-1), (length//2)-1, not is_even)
-    # bottom right
     just_outside += calc_area((w-1,h-1), (length//2)-1, not is_even)
-    # top right
     just_outside += calc_area((w-1,0), (length//2)-1, not is_even)
-    # top left
     just_outside += calc_area((0,0), (length//2)-1, not is_even)
     just_outside *= field_cycles
 
     # subtract non-filled parts of the outer head fields
     head_subtract = 0
-    sub = odd
-    # top
+    sub = even if is_even else odd
+    # top, bottom, right, left
     head_subtract += sub - calc_area((w//2,h-1), length-1, not is_even)
-    # bottom
     head_subtract += sub - calc_area((w//2,0), length-1, not is_even)
-    # right
     head_subtract += sub - calc_area((0,h//2), length-1, not is_even)
-    # left
     head_subtract += sub - calc_area((w-1,h//2), length-1, not is_even)
 
     # subtract non-filled parts of the outer corner fields
     corner_subtract = 0
     sub = even if is_even else odd
-    # bottom left
+    # bottom left, bottom right, top right, top left
     corner_subtract += sub - calc_area([(0,h//2), (w//2,h-1)], length-1, not is_even)
-
-    # bottom right
     corner_subtract += sub - calc_area([(w-1,h//2), (w//2,h-1)], length-1, not is_even)
-    # top right
     corner_subtract += sub - calc_area([(w-1,h//2), (w//2,0)], length-1, not is_even)
-    # top left
     corner_subtract += sub - calc_area([(0,h//2), (w//2,0)], length-1, not is_even)
     corner_subtract *= field_cycles-1
 
